18c95c458df6b2beb14b7162033c0d9cf465ce5d
Marcelo Divaldo Brake <contato@marcelobrake.com.br>
2015-09-11 22:41:02 -0300

    Aula 11/09 Monolog e SLIM

--- public/index.php
+++ public/index.php
@@ -6,6 +6,13 @@ $app = new Slim\Slim();
 
 $app->get("/", function () {
     echo "Pagina inicial";
+
+    $logger = new Monolog\Logger("acesso_barra");
+    $logger->pushHandler(new Monolog\Handler\StreamHandler("../logs/acessos.log"));
+    $logger->addInfo("O IP $_SERVER[REMOTE_ADDR] acessou a rota /");
+    
 });
+
+
 
 $app->run();
